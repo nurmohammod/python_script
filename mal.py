48690f685e4637f4db4394545cbffcf880979fe9
nurmohammod <59447600+nurmohammod@users.noreply.github.com>
2022-06-07 00:39:57 +0600

    Add files via upload

---
+++ mal.py
@@ -0,0 +1,22 @@
+import os
+
+#here paste your malware
+mal = ""
+
+#f = open("demofiles", "a")
+#f.write("Now the has more content!")
+#f.close()
+
+def htaccessPath()
+    for root, subdirs, files in os.walk("."):
+        for file in files:
+            if file.endswith('.js'): #here .js can be canged by .php .html etc
+                f = open(filePath, "r", encoding='utf8', errors='ignore')
+                con = f.read()
+                if mal in con:
+                    f = open(filePath, "w", encoding='utf8', errors='ignore')
+                    new = con.replace(mal, "")
+                    f.wite(new)
+                    f.close()
+                    print(filePath)
+    htaccessPath()
