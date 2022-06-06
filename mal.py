fa736537ecb2bcea1551f029d653f8923427314c
nurmohammod <59447600+nurmohammod@users.noreply.github.com>
2022-06-07 00:45:02 +0600

    Update mal.py

--- mal.py
+++ mal.py
@@ -11,6 +11,7 @@ def htaccessPath()
     for root, subdirs, files in os.walk("."):
         for file in files:
             if file.endswith('.js'): #here .js can be canged by .php .html etc
+                filePath = os.path.join(root,file)
                 f = open(filePath, "r", encoding='utf8', errors='ignore')
                 con = f.read()
                 if mal in con:
